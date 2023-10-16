39c6d92f20b695da1af72690bca61d48b79baf8c
jay patel <106934952+pateljay9936@users.noreply.github.com>
2023-10-16 14:52:36 -0400

    from Mac

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+p_tag = document.getElementById('content_p')
+
+date = new Date().toDateString()
+time = new Date().toLocaleTimeString()
+
+
+fun_date_time = () => {
+    date = new Date().toDateString()
+    time = new Date().toLocaleTimeString()
+    console.log(time+" on "+date)
+    p_tag.innerHTML = `${time}  on  ${date}`   
+}
+
+setInterval(fun_date_time, 1000)
