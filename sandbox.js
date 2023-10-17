991dd4bfca6a32ecaf32034832a71634d68f5f54
jay_patel <pateljay9936@gmail.com>
2023-10-17 16:24:00 -0400

    continuation to last commit and little bit improvement in code

--- sandbox.js
+++ sandbox.js
@@ -1,41 +1,16 @@
 const addButton = document.getElementById("add-btn");
 const titleInput = document.getElementById("title-input");
 const discInput = document.getElementById("disc-input");
+const clrbutton = document.getElementById("clr-btn");
 
-function update(flag) {
+function update() {
   console.log("Updating List...");
 
-  const titleValue = titleInput.value.trim();
-  const discValue = discInput.value.trim();
-  console.log("Title Value: ", titleValue);
-  console.log("Description Value: ", discValue);
- 
- 
-  
-  
-  if ((titleValue === "" || discValue === "") && flag=='1'){
-    
-    console.log("Update Aborted...");
-    return; // exit the function if either input field is empty
-  }
- itemJsonArray = [];
+  itemJsonArray = [];
 
-  if (localStorage.getItem("itemsJson") == null) {
-   
-    itemJsonArray.push([titleInput.value, discInput.value]);
-    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
-    }
-   else {
-    itemJsonArrayStr = localStorage.getItem("itemsJson");
-    itemJsonArray = JSON.parse(itemJsonArrayStr);
-    if (flag=='1')
-    {
-    itemJsonArray.push([titleInput.value, discInput.value]);
-    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
-    }
-  }
+  itemJsonArrayStr = localStorage.getItem("itemsJson");
+  itemJsonArray = JSON.parse(itemJsonArrayStr);
 
-  
   let tableBody = document.getElementById("tableBody");
   let str = "";
   itemJsonArray.forEach((element, index) => {
@@ -44,34 +19,77 @@ function update(flag) {
         <td scope="row">${index + 1}</td>
         <td>${element[0]}</td>
         <td>${element[1]}</td>
-        <td><button onclick="deleteItem(${index +1})"  class="btn btn-sm btn-primary">Delete</button></td>
+        <td><button onclick="deleteItem(${
+          index + 1
+        })"  class="btn btn-sm btn-primary">Delete</button></td>
+      </tr>`;
+  });
+  tableBody.innerHTML = str;
+}
+
+function updateItem() {
+  console.log("Updating List with new values...");
+
+  itemJsonArray = [];
+
+  if (localStorage.getItem("itemsJson") == null) {
+    itemJsonArray.push([titleInput.value, discInput.value]);
+    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+  } else {
+    itemJsonArrayStr = localStorage.getItem("itemsJson");
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+
+    itemJsonArray.push([titleInput.value, discInput.value]);
+    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+  }
+
+  let tableBody = document.getElementById("tableBody");
+  let str = "";
+  itemJsonArray.forEach((element, index) => {
+    str += `
+      <tr>
+        <td scope="row">${index + 1}</td>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td>
+        <td><button onclick="deleteItem(${
+          index + 1
+        })"  class="btn btn-sm btn-primary">Delete</button></td>
       </tr>`;
   });
   tableBody.innerHTML = str;
 }
 
 function handleAddButtonClick() {
-  update("1");    //flag 1 for add button, where it will add the data to local storage and print table
-}
-addButton.addEventListener("click", handleAddButtonClick);
+  const titleValue = titleInput.value.trim();
+  const discValue = discInput.value.trim();
+  console.log("Title Value: ", titleValue);
+  console.log("Description Value: ", discValue);
 
-update("0");  //flag 0 for page load, where it will not add the data to local storage just print table
-              // On page load, retrieve data from local storage and populate the table
+  if (titleValue == "" || discValue == "") {
+    console.log("Update Aborted...");           //add button click event handler
+    return;
+  }
+  updateItem();
+  titleInput.value = "";
+  discInput.value = "";
+}
+addButton.addEventListener("click", handleAddButtonClick);    //add buton click event
+
+clrbutton.addEventListener("click", () => {                            
+  localStorage.clear();                            //clear button click event
+  itemJsonArray = [];
+  localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+  update();
+});
 
 function deleteItem(index) {
   console.log("Delete", index);
   itemJsonArrayStr = localStorage.getItem("itemsJson");
-  itemJsonArray = JSON.parse(itemJsonArrayStr);
+  itemJsonArray = JSON.parse(itemJsonArrayStr);           //delete button click event handler
   // Delete itemIndex element from the array
-  itemJsonArray.splice(index-1, 1);
+  itemJsonArray.splice(index - 1, 1);
   localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
-  update("0");
+  update();
 }
 
-function handleAddButtonClick() {
-  update("1");
-  titleInput.value = "";
-  discInput.value = "";
-}
-
-addButton.addEventListener("click", handleAddButtonClick);
+update(); //update the table on page load
